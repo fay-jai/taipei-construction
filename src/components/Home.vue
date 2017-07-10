e61135e8f88b9117bfff9f45f70c839e7106d928
Willson Mock <willson.mock@gmail.com>
2017-07-09 21:54:39 -0700

    Add styles for contact info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -32,14 +32,14 @@
         </div>
         <div class="info">
           <div class="phone">
-            <div v-if="lang === 'en'">Phone</div>
-            <div v-else>电话</div>
+            <div class="text" v-if="lang === 'en'">Phone</div>
+            <div class="text" v-else>电话</div>
             <div>408-887-3345 (Jim Wen)</div>
             <div>408-398-2831 (Lily Jiang)</div>
           </div>
           <div class="email">
-            <div v-if="lang === 'en'">Email</div>
-            <div v-else>邮箱</div>
+            <div class="text" v-if="lang === 'en'">Email</div>
+            <div class="text" v-else>邮箱</div>
             <a href="mailto:taipeiconstruction.jn@gmail.com">taipeiconstruction.jn@gmail.com</a>
           </div>
         </div>
@@ -62,12 +62,55 @@ export default {
 </script>
 
 <style>
+.container {
+  background-color: white;
+  border: 4px solid darkred;
+  line-height: 2.5;
+  margin: 40px auto;
+  max-width: 800px;
+  padding: 20px 40px;
+}
+
+.contact {
+  display: flex;
+  margin: 0 auto;
+  width: 450px;
+}
+
 .home {
   padding: 10px;
+}
+
+.info {
+  padding: 20px;
+  width: calc(100% - 150px);
+}
+
+.intro {
+  margin-bottom: 40px;
+}
+
+.name {
+  text-align: center;
 }
 
 .photo img {
   width: 150px;
   height: 150px;
+}
+
+.profile {
+  padding: 20px;
+  width: 150px;
+}
+
+.text {
+  text-decoration: underline;
+}
+
+@media screen and (max-width: 550px) {
+  .contact {
+    flex-direction: column;
+  }
 }
 </style>
